f44658e863abd88d9ea22d48b3f2f0d9b1225992
shubhamg <shubham.gupta30@gmail.com>
2013-10-03 19:11:01 -0700

    Adding unit tests

--- Graph/Graph_test.py
+++ Graph/Graph_test.py
@@ -1,13 +1,29 @@
-
+"""
+Test file for Graph.py
+"""
+import unittest
 from Graph import Graph
 from Graph import Edge
 from Graph import Vertex
 
-v = Vertex('v')
-w = Vertex('w')
-y = Vertex('y')
-x = Vertex('v')
+class GraphTest(unittest.TestCase):
 
-g = Graph([v,w,y,x])
-print g
+  def equal(self, g1, g2):
+    """ Tests if two graphs are same
+    """
+    return g1.__dict__ == g2.__dict__
+
+  def test_add_vertex(self):
+    g1 = Graph()
+    g1.add_vertex(Vertex('label1'))
+    g1.add_vertex(Vertex('label2'))
+    g1.add_vertex(Vertex('label2'))
+
+    g2 = Graph()
+    g2.add_vertex(Vertex('label1'))
+    g2.add_vertex(Vertex('label2'))
+    self.assertTrue(self.equal(g1, g2))
+
+if __name__ == "__main__":
+  unittest.main()
 
