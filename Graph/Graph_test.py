7f765d07b45a460f3cd7692b884f3e075330819a
shubhamg <shubham.gupta30@gmail.com>
2013-10-04 13:41:33 -0700

    Unit Testing

--- Graph/Graph_test.py
+++ Graph/Graph_test.py
@@ -9,22 +9,28 @@ from sets import Set
 
 class GraphTest(unittest.TestCase):
 
-  def equal(self, g1, g2):
+  def isomorphic(self, g1, g2):
     """ Tests if two graphs are same
     """
     return g1.__dict__ == g2.__dict__
 
   def test_add_vertex(self):
     g = Graph()
+    # Tests that a label is used only once.
     g.add_vertex(Vertex('label1'))
     g.add_vertex(Vertex('label2'))
     g.add_vertex(Vertex('label2'))
 
-    self.assertEqual(Vertex('l'), Vertex('l'))
+    self.assertEqual(g, {Vertex('label1'):{}, Vertex('label2'):{}})
     self.assertEqual(g.vertex_labels, Set(['label1', 'label2']))
-    self.assertEqual(sorted(g.keys()), [Vertex('label1'), Vertex('label2')])
-    self.assertEqual(g[Vertex('label1')], {})
-    self.assertEqual(g[Vertex('label2')], {})
+
+  def test_add_edge_without_adding_vertices(self):
+    g = Graph()
+    g.add_edge(Edge(Vertex('label1'), Vertex('label2')))
+
+    self.assertEqual(g, {})
+    self.assertEqual(g.vertex_labels, Set())
+
 
 if __name__ == "__main__":
   unittest.main()
